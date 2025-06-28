a2332bc5928784133efd43717c9b704fa437f388
ch1ka <69473073+JI0shka@users.noreply.github.com>
2025-06-29 01:33:44 +0300

    create chapters.ts

---
+++ src/data/chapters.ts
@@ -0,0 +1,63 @@
+export interface Chapter {
+  number: number;
+  title: string;
+  url: string; 
+}
+
+export const chapters: Chapter[] = [
+  {
+    number: 0,
+    title: "Заклятие Кошмара",
+    url: '/chapters/0.md',
+  },
+  {
+    number: 1,
+    title: "Кошмар начинается",
+    url: '/chapters/1.md',
+  },
+  {
+    number: 2,
+    title: "Караван рабов",
+    url: '/chapters/2.md',
+  },
+  {
+    number: 3,
+    title: "Нити судьбы",
+    url: '/chapters/3.md',
+  },
+  {
+    number: 4,
+    title: "Горный Король",
+    url: '/chapters/4.md',
+  },
+  {
+    number: 5,
+    title: "Разорванный цепи",
+    url: '/chapters/5.md',
+  },
+  {
+    number: 6,
+    title: "Противостояние Тирану",
+    url: '/chapters/6.md',
+  },
+  {
+    number: 7,
+    title: "Три раба и Герой",
+    url: '/chapters/7.md',
+  },
+  {
+    number: 8,
+    title: "Совсем ничего",
+    url: '/chapters/8.md',
+  },
+  {
+    number: 9,
+    title: "Принятие желаемого за действительное",
+    url: '/chapters/9.md',
+  },
+  {
+    number: 10,
+    title: "Первый павший",
+    url: '/chapters/10.md',
+  },
+]; 
