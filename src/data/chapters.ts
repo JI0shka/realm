ff089d4059e3e366f8bb796f094693550f3b5929
ch1ka <69473073+JI0shka@users.noreply.github.com>
2025-06-29 02:00:08 +0300

    Update chapters.ts

--- src/data/chapters.ts
+++ src/data/chapters.ts
@@ -8,56 +8,56 @@ export const chapters: Chapter[] = [
   {
     number: 0,
     title: "Заклятие Кошмара",
-    url: '/chapters/0.md',
+    url: 'https://zyeedsomlodfecw8.public.blob.vercel-storage.com/0.md',
   },
   {
     number: 1,
     title: "Кошмар начинается",
-    url: '/chapters/1.md',
+    url: 'https://zyeedsomlodfecw8.public.blob.vercel-storage.com/1.md',
   },
   {
     number: 2,
     title: "Караван рабов",
-    url: '/chapters/2.md',
+    url: 'https://zyeedsomlodfecw8.public.blob.vercel-storage.com/2.md',
   },
   {
     number: 3,
     title: "Нити судьбы",
-    url: '/chapters/3.md',
+    url: 'https://zyeedsomlodfecw8.public.blob.vercel-storage.com/3.md',
   },
   {
     number: 4,
     title: "Горный Король",
-    url: '/chapters/4.md',
+    url: 'https://zyeedsomlodfecw8.public.blob.vercel-storage.com/4.md',
   },
   {
     number: 5,
     title: "Разорванный цепи",
-    url: '/chapters/5.md',
+    url: 'https://zyeedsomlodfecw8.public.blob.vercel-storage.com/5.md',
   },
   {
     number: 6,
     title: "Противостояние Тирану",
-    url: '/chapters/6.md',
+    url: 'https://zyeedsomlodfecw8.public.blob.vercel-storage.com/6.md',
   },
   {
     number: 7,
     title: "Три раба и Герой",
-    url: '/chapters/7.md',
+    url: 'https://zyeedsomlodfecw8.public.blob.vercel-storage.com/7.md',
   },
   {
     number: 8,
     title: "Совсем ничего",
-    url: '/chapters/8.md',
+    url: 'https://zyeedsomlodfecw8.public.blob.vercel-storage.com/8.md',
   },
   {
     number: 9,
     title: "Принятие желаемого за действительное",
-    url: '/chapters/9.md',
+    url: 'https://zyeedsomlodfecw8.public.blob.vercel-storage.com/9.md',
   },
   {
     number: 10,
     title: "Первый павший",
-    url: '/chapters/10.md',
+    url: 'https://zyeedsomlodfecw8.public.blob.vercel-storage.com/10.md',
   },
 ]; 
